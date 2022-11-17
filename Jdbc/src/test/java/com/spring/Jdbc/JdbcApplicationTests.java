1fa412da133260127ec8e733b27cb515bf659c23
Harry <86228650+Hari-developer@users.noreply.github.com>
2022-11-17 18:49:57 +0530

    Add files via upload

---
+++ Jdbc/src/test/java/com/spring/Jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
